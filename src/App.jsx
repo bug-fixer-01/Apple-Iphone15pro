aa187fcdbf4af55339f7aab31c8eea48bb9ab825
prayash <prayashsingh608@gmail.com>
2025-08-03 15:37:38 +0530

    Highlight section completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Highlights from './components/Highlights'
+import gsap from 'gsap'
+import { ScrollTrigger } from 'gsap/all'
 
+gsap.registerPlugin(ScrollTrigger);
 const App = () => {
   return (
    <main className='bg-black'>
       <Navbar/>
       <Hero/>
+      <Highlights/>
    </main>
   )
 }
--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -1,12 +1,31 @@
-import React from 'react'
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import VideoCarousel from './VideoCarousel'
+import {playImg, rightImg, watchImg} from '../utils'
+
 
 const Highlights = () => {
+  useGSAP(()=>{
+      gsap.to('#title', {opacity:1, y:0})
+      gsap.to('.link', {opacity:1, y:0 , stagger:0.2 ,duration:1})
+  },[])
   return (
-    <section>
-        <div>
-            <div>
-                <h1>Get the Highlights.</h1>
+    <section id='highlights' className='w-screen h-full overflow-hidden common-padding bg-zinc'>
+        <div className='screen-max-width'>
+            <div className='mb-12 w-full md:flex items-end justify-between'>
+                <h1 id='title' className='section-heading'>Get the Highlights.</h1>
+                <div className='flex flex-wrap items-end gap-5'>
+                  <p className='link'>
+                    watch the film
+                    <img className='w-4 h-4 ml-2' src={watchImg} alt="watch" />
+                  </p>
+                  <p className='link'>
+                    watch the event
+                    <img className='ml-2' src={rightImg} alt="watch"/>
+                  </p>  
+                </div>
             </div>
+            <VideoCarousel />
         </div>
     </section>
   )
